f705ad1d4e7aaa8ad75d8f31e5427b3b9bfda7a5
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-11 16:46:39 +0300

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+     if(t<0){
+      t=t+t*-2;}
      int sum=0;
      String str=Integer.toString(t);
      for(int i=0;i<str.length();i++){
