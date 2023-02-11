064c57b1456f8a892290449ac88cfa6119153804
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-11 16:01:28 +0300

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+     int number1=0;
+     int number2=1;
+     System.out.println(number1);
+     System.out.println(number2);
+     for(int i=3;i<=lastFibonacci;i++){
+      int number3=number1+number2;
+      System.out.println(number3);
+      number1=number2;
+      number2=number3;} 
     }
 }
