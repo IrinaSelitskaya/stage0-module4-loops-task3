37afc0b10a23b2643887281e0ab453e5fdec1ef7
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-11 22:15:44 +0300

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-     char a='A';
-     char z='Z';
+     char a='a';
+     char z='z';
      while(a<=z){
       System.out.println(a);
       a++;}
